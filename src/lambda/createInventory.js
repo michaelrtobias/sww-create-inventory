2027962ba9aaabe6f2c5ed9166be939840d61864
Michael Tobias <michaelremitobias@gmail.com>
2022-08-02 22:00:56 -0700

    can add images

--- src/lambda/createInventory.js
+++ src/lambda/createInventory.js
@@ -1,29 +1,46 @@
-// const createItem = require("../dynamodb/createItem");
-// const formatParams = (body) => {
-//   let params = {};
-//   const keys = Object.keys(body);
-//   keys.forEach((key) => {
-//     params[key] = {
-//       S: body[key],
-//     };
-//   });
-//   // do something with images
-//   params = {
-//     ...params,
-//     timestamp: {
-//       S: new Date().toISOString(),
-//     },
-//   };
+const createItem = require("../dynamodb/createItem");
+const formatParams = (body) => {
+  let params = {};
+  const keys = Object.keys(body);
+  keys.forEach((key) => {
+    params[key] = {
+      S: body[key],
+    };
+  });
 
-//   return params;
-// };
+  const formattedImages = body.images.map((image) => ({
+    M: {
+      image_url: {
+        S: image,
+      },
+      alt: {
+        S: `${body.brand.toLowerCase()}-${body.model.toLowerCase()}-${body.model_number.toLowerCase()}`,
+      },
+    },
+  }));
+  // do something with images
+  params = {
+    ...params,
+    timestamp: {
+      S: new Date().toISOString(),
+    },
+    colorway: {
+      S: `${body.model_number.toLowerCase()}-${body.dial.toLowerCase()}-${body.bezel.toLowerCase()}`,
+    },
+    images: {
+      L: formattedImages,
+    },
+  };
+
+  return params;
+};
 
 module.exports = async (body) => {
-  // let params = {
-  //   Item: formatParams(body),
-  // };
-  // const result = await createItem(params, "leads");
-  const result = "creating items with the cure";
-  console.log("body", body);
+  let params = {
+    Item: formatParams(body),
+    ConditionExpression: "attribute_not_exists(colorway)",
+  };
+  console.log("params", params);
+  const result = await createItem(params, "watchInventory");
   return result;
 };
